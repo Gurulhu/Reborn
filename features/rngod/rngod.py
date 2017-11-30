6dcbf6ddede2d5751d4b04418d99bf80cbb1ddac
Gurulhu <lucasag@hotmail.com.br>
2017-11-30 10:13:55 -0200

    Minor fixes in RNG Working on a webscrapping feature for MTG card pricing.

--- features/rngod/rngod.py
+++ features/rngod/rngod.py
@@ -10,13 +10,16 @@ def reply( query ):
   print("RNG")
   token = query["qcontent"].split()
   reply = "None", None
-  if( len( token ) == 4 ):
-      reply = str( random.randrange( float( token[1] ), float( token[2] ), float( token[3] ) ) )
-  elif( len( token ) == 3 ):
-      reply = str( random.randrange( float( token[1] ), float( token[2] ) ) )
-  elif( len( token ) == 2 ):
-      reply = str( random.randrange( float( token[1] ) ) )
-  else:
+  try:
+      if( len( token ) == 4 ):
+          reply = str( random.randrange( float( token[1] ), float( token[2] ), float( token[3] ) ) )
+      elif( len( token ) == 3 ):
+          reply = str( random.randrange( float( token[1] ), float( token[2] ) ) )
+      elif( len( token ) == 2 ):
+          reply = str( random.randrange( float( token[1] ) ) )
+      else:
+          reply = help()
+  except:
       reply = help()
 
   rinterface = query["qinterface"]
